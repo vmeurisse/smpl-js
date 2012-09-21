fe9b88f55402ef5c5f4f729c33282d384353618b
Vincent Meurisse <dev@meurisse.org>
2012-09-21 21:19:20 +0200

    Improve smpl.utils.strinfy.
    not based on JSON.stringify anymore for greater flexibility

--- src/smpl.utils.js
+++ src/smpl.utils.js
@@ -18,16 +18,44 @@ define(['./smpl.core'], function(smpl) {
 				.replace(/\//g, '\\/');
 	};
 	
-	var replacer = function(key, value) {
-		if (value === undefined) {
+	var indent = function (array, level, chars) {
+		if (!array.length) {
+			return chars;
+		}
+		var inlevel = level + '\t';
+		return chars.charAt(0) + '\n' + inlevel + array.join(',\n' + inlevel) + '\n' + level + chars.charAt(1);
+	};
+	
+	var stringify = function(value, stringifyStack, level) {
+		
+		if (value === undefined || value === null) {
 			return '' + value;
 		}
-		if (typeof value === 'number' && (isNaN(value) || !isFinite(value))) {
+		
+		if (typeof value === 'number') {
+			if (value === 0 && 1/value === -Infinity) {
+				return '-0';
+			}
 			return value.toString();
 		}
+		
 		if (typeof value === 'function' || value instanceof RegExp) {
 			return value.toString();
 		}
+		
+		if (typeof value === 'string' || typeof value === 'boolean') {
+			return JSON.stringify(value);
+		}
+		
+		var proto = Object.prototype.toString.call(value);
+		if (proto === '[object Boolean]' || proto === '[object Number]' || proto === '[object String]') {
+			return proto.slice(8, -1) + '(' + stringify(value.valueOf(), [], 0) + ')';
+		}
+		
+		if (proto === '[object Date]') {
+			return 'new Date("' + value.toString() + '")';
+		}
+		
 		if (typeof value === 'object') {
 			var i = stringifyStack.length;
 			while (i--) {
@@ -36,32 +64,39 @@ define(['./smpl.core'], function(smpl) {
 				}
 			}
 			stringifyStack.push(value);
-		}
-		if (Array.isArray(value)) {
-			var cleanArray = [];
-			var lastIndex = -1;
-			for (var k in value) {
-				var index = +k;
-				if (!isNaN(index)) { //skip stupid non-array values stored in array. (eg. var a = []; a.stupid = true;)
-					if (index > lastIndex + 1) {
-						cleanArray.push('undefined x ' + (lastIndex - index));
+			
+			if (Array.isArray(value)) {
+				var cleanArray = [];
+				var lastIndex = -1;
+				for (var k in value) {
+					var index = +k;
+					if (!isNaN(index)) { //skip stupid non-array values stored in array. (eg. var a = []; a.stupid = true;)
+						if (index > lastIndex + 1) {
+							cleanArray.push('undefined x ' + (index - lastIndex - 1));
+						}
+						cleanArray.push(stringify(value[k], stringifyStack, level + '\t'));
+						lastIndex = index;
 					}
-					cleanArray.push(smpl.utils.stringify(value[k], stringifyStack));
-					lastIndex = index;
 				}
+				if (lastIndex < value.length - 1) {
+					cleanArray.push('undefined x ' + (value.length - lastIndex - 1));
+				}
+				return indent(cleanArray, level, '[]')
+			} else {
+				var str = [];
+				var keys = Object.keys(value).sort(); //Make sure we get reproducible results
+				for (var i = 0, l = keys.length; i < l; i++) {
+					var k = keys[i];
+					var v = stringify(value[k], stringifyStack, level + '\t');
+					str.push(JSON.stringify(k) + ': ' + v);
+				}
+				return indent(str, level, '{}')
 			}
-			if (lastIndex < value.length - 1) {
-				cleanArray.push('undefined x ' + (value.length - lastIndex - 1));
-			}
-			return '[' + cleanArray.join(', ') + ']';
 		}
-		return value;
+		return '' + value;
 	};
-	var stringifyStack = [];
-	
-	smpl.utils.stringify = function(object, stack) {
-		stringifyStack = stack || [];
-		return JSON.stringify(object, replacer, '\t');
+	smpl.utils.stringify = function(object) {
+		return stringify(object, [], '');
 	};
 	
 	return smpl;
---
+++ test/smpl.utils.js
@@ -0,0 +1,55 @@
+var assert = require('../assert');
+var smpl = require('../smpl.utils');
+suite('smpl.utils', function() {
+	suite('stringify', function() {
+		test('simple', function() {
+			assert.equals(smpl.utils.stringify({}), '{}');
+			assert.equals(smpl.utils.stringify([]), '[]');
+			assert.equals(smpl.utils.stringify(''), '""');
+			assert.equals(smpl.utils.stringify(1), '1');
+			assert.equals(smpl.utils.stringify(-0), '-0');
+			assert.equals(smpl.utils.stringify(), 'undefined');
+			assert.equals(smpl.utils.stringify(null), 'null');
+			assert.equals(smpl.utils.stringify(/s/g), '/s/g');
+ 			assert.equals(smpl.utils.stringify(new Date(0)), 'new Date("' + (new Date(0)).toString() + '")');
+		});
+		test('array', function() {
+			assert.equals(smpl.utils.stringify([1, 2, "3"]), '[\n\t1,\n\t2,\n\t"3"\n]');
+		});
+		test('sparse array', function() {
+			assert.equals(smpl.utils.stringify([undefined, undefined]), '[\n\tundefined,\n\tundefined\n]');
+			var sparse = [];
+			sparse[2] = 1;
+			sparse[10] = undefined; //Test that manualy inserted values are not like missing ones
+			sparse.length = 1000;
+			sparse.badProperty = 'x';
+			var sparseString = '[\n\tundefined x 2,\n\t1,\n\tundefined x 7,\n\tundefined,\n\tundefined x 989\n]';
+			assert.equals(smpl.utils.stringify(sparse), sparseString);
+		});
+		test('object', function() {
+			assert.equals(smpl.utils.stringify({a: 1, 2: 3}), '{\n\t"2": 3,\n\t"a": 1\n}');
+			
+			// We do not stringify the prototype
+			var o = Object.create({a: 1});
+			o.p = 'x';
+			assert.equals(smpl.utils.stringify(o), '{\n\t"p": "x"\n}');
+		});
+		test('embed', function() {
+			assert.equals(smpl.utils.stringify({a: {b: 1}}), '{\n\t"a": {\n\t\t"b": 1\n\t}\n}');
+			assert.equals(smpl.utils.stringify([{a: 1}]), '[\n\t{\n\t\t"a": 1\n\t}\n]');
+			assert.equals(smpl.utils.stringify({a: [1, 2]}), '{\n\t"a": [\n\t\t1,\n\t\t2\n\t]\n}');
+			assert.equals(smpl.utils.stringify([[1, 2]]), '[\n\t[\n\t\t1,\n\t\t2\n\t]\n]');
+		});
+		test('typed wrapper', function() {
+			// One advice: never ever use that !!!
+			assert.equals(smpl.utils.stringify(new String('test')), 'String("test")');
+			assert.equals(smpl.utils.stringify(new Number(7)), 'Number(7)');
+			assert.equals(smpl.utils.stringify(new Boolean(true)), 'Boolean(true)');
+		});
+		test('circular', function() {
+			var o = {};
+			o.a = o;
+			assert.equals(smpl.utils.stringify(o), '{\n\t"a": circular reference\n}');
+		});
+	});
+});
