2dc7cc27d869dd4c1adf48bccfd236426a0ac49d
Vincent Meurisse <dev@meurisse.org>
2012-10-20 19:29:48 +0200

    Improve smpl.Ecb
    * new methods pause/resume and reset
    * code is now unit tested

--- src/smpl.Ecb.js
+++ src/smpl.Ecb.js
@@ -1,8 +1,16 @@
 define(['./smpl.core'], function(smpl) {
+	'use strict';
+	
 	smpl.Ecb = function() {
-		this.callbacks = {};
+		this.reset();
 	};
 
+	smpl.Ecb.prototype.reset = function() {
+		this.callbacks = {};
+		this.paused = 0;
+		this.deferred = [];
+	};
+	
 	smpl.Ecb.prototype.addListener = function(event, fn, scope) {
 		if (!this.callbacks[event]) {
 			this.callbacks[event] = [];
@@ -21,21 +29,42 @@ define(['./smpl.core'], function(smpl) {
 		var i = callbacks.length;
 		while (i--) {
 			var cb = callbacks[i];
-			if (cb.fn === fn && (arguments.length < 3 || cb.scope === scope)) {
+			if ((fn === undefined || cb.fn === fn) && (scope === undefined || cb.scope === scope)) {
 				callbacks.splice(i, 1);
 			}
 		}
 	};
 	
 	smpl.Ecb.prototype.fire = function(event) {
-		var callbacks = this.callbacks[event];
-		if (callbacks) {
-			for (var i = 0, l = callbacks.length; i < l; i++) {
-				var cb = callbacks[i];
-				cb.fn.apply(cb.scope, arguments);
+		if (this.paused) {
+			this.deferred.push(arguments);
+		} else {
+			var callbacks = this.callbacks[event];
+			if (callbacks) {
+				for (var i = 0, l = callbacks.length; i < l; i++) {
+					var cb = callbacks[i];
+					cb.fn.apply(cb.scope, arguments);
+				}
 			}
 		}
 	};
-
+	
+	smpl.Ecb.prototype.pause = function() {
+		this.paused++;
+	};
+	
+	smpl.Ecb.prototype.resume = function() {
+		if (this.paused) {
+			this.paused--;
+			if (!this.paused) {
+				var length = this.deferred.length;
+				for (var i = 0; i < length; i++) {
+					this.fire.apply(this, this.deferred[i]);
+				}
+				this.deferred.splice(0, length);
+			}
+		}
+	};
+	
 	return smpl;
 });
