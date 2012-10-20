9b7b146bafb72830015bdd34c8db20d1245e642a
Vincent Meurisse <dev@meurisse.org>
2012-10-21 00:40:31 +0200

    Add documentation to smpl.string

--- src/smpl.string.js
+++ src/smpl.string.js
@@ -1,6 +1,16 @@
 define(['./smpl.core'], function(smpl) {
 	smpl.string = {};
 	
+	/**
+	 * Replace patterns inside a string with the coresponding value
+	 * 
+	 *     smpl.string.supplant('test {method}', {method: 'supplant'}); // => 'test supplant'
+	 *     smpl.string.supplant('{0} {1} {2}', ['work', 'with', 'arrays']); // => 'work with arrays'
+	 * 
+	 * @param {String} string The string to operate on
+	 * @param {Object} object The object where the replacement are taken from
+	 * @return {String} the result of the replacement
+	 */
 	smpl.string.supplant = function(string, object) {
 		return string.replace(/\{(\w+)\}/g, function(a, key) {
 			var replacer = object[key];
@@ -8,8 +18,15 @@ define(['./smpl.core'], function(smpl) {
 		});
 	};
 	
+	/**
+	 * Test if a string starts with the given substring
+	 * @param {String} string The string to test
+	 * @param {String} start  The substring to search
+	 * @return {Boolean} true if `string` starts with `start`. False otherwise
+	 */
 	smpl.string.startsWith = function(string, start) {
 		return string.substr(0, start.length) === start;
 	};
+	
 	return smpl;
 });
