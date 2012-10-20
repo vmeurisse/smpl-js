ad63d24e4ef087699e4ac0573f42682c13915d3b
Vincent Meurisse <dev@meurisse.org>
2012-10-20 23:52:56 +0200

    Add documentation for smpl.Ecb

--- src/smpl.Ecb.js
+++ src/smpl.Ecb.js
@@ -1,16 +1,31 @@
 define(['./smpl.core'], function(smpl) {
 	'use strict';
 	
+	/**
+	 * smpl.Ecb is a simple Event Communication Bus
+	 * It provide a simple addListener/fire messaging system
+	 * @return a new instance of smpl.Ecb.
+	 */
 	smpl.Ecb = function() {
 		this.reset();
 	};
-
+	
+	/**
+	 * Reset the ecb. All listener are removed, pending fire are deleted and the ecb is resumed.
+	 */
 	smpl.Ecb.prototype.reset = function() {
 		this.callbacks = {};
 		this.paused = 0;
 		this.deferred = [];
 	};
 	
+	/**
+	 * Add a listener to an event
+	 * 
+	 * @param {String} event Name of the event to listen to
+	 * @param {Function} fn  Function to be called
+	 * @param {Object} scope Scope under which the `fn` is called (optional)
+	 */
 	smpl.Ecb.prototype.addListener = function(event, fn, scope) {
 		if (!this.callbacks[event]) {
 			this.callbacks[event] = [];
@@ -21,6 +36,12 @@ define(['./smpl.core'], function(smpl) {
 		});
 	};
 	
+	/**
+	 * Remove one or multiple listeners.
+	 * @param {String} event Name of the event
+	 * @param {Function} fn  Remove only listeners whose function is equal to `fn`. Ignored is equal to undefined (optional)
+	 * @param {Object} scope Remove only listeners whose scope is equal to `fn`. Ignored is equal to undefined (optional)
+	 */
 	smpl.Ecb.prototype.removeListener = function(event, fn, scope) {
 		var callbacks = this.callbacks[event];
 		if (!callbacks) {
@@ -35,6 +56,16 @@ define(['./smpl.core'], function(smpl) {
 		}
 	};
 	
+	/**
+	 * fire an event. Every listener associated to the event will be called in the order they where registered
+	 * Any parameter of `fire` will be passed to the listeners
+	 * 
+	 *     var ecb = new smpl.Ecb();
+	 *     ecb.addListener('log', console.log, console);
+	 *     ecb.fire('log', 1, 2); // => 'log' 1 2
+	 * 
+	 * @param {String} event Name of the event
+	 */
 	smpl.Ecb.prototype.fire = function(event) {
 		if (this.paused) {
 			this.deferred.push(arguments);
@@ -49,10 +80,17 @@ define(['./smpl.core'], function(smpl) {
 		}
 	};
 	
+	/**
+	 * Pause the Ecb. Any call to fire will be delayed until a call to `resume`.
+	 * If pause is called multiple time, the same number of call must be done to `resume` to actually resume the Ecb.
+	 */
 	smpl.Ecb.prototype.pause = function() {
 		this.paused++;
 	};
 	
+	/**
+	 * Resume the Ecb. (@see smpl.Ecb.prototype.pause)
+	 */
 	smpl.Ecb.prototype.resume = function() {
 		if (this.paused) {
 			this.paused--;
